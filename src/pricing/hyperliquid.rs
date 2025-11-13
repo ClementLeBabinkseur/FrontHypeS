2176321fd1b65d22e2353141ca6c15eb0bc5d873
Ostad <nicolas.delisee@gmail.com>
2025-11-14 05:37:40 +0900

    comment HL stream for cleaner feed

--- src/pricing/hyperliquid.rs
+++ src/pricing/hyperliquid.rs
@@ -144,10 +144,10 @@ impl HyperliquidPriceFetcher {
                 prices_write.insert(data.coin.clone(), price.clone());
                 
                 // Commented out for cleaner logs - prices are visible in snapshots
-                 info!(
-                     "Hyperliquid {} - Bid: {:.2} | Mid: {:.2} | Ask: {:.2}",
-                     data.coin, best_bid, mid_price, best_ask
-                 );
+                // info!(
+                //     "Hyperliquid {} - Bid: {:.2} | Mid: {:.2} | Ask: {:.2}",
+                //     data.coin, best_bid, mid_price, best_ask
+                // );
             }
         }
         
